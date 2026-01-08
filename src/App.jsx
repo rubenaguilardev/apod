e0770b40bff0980184f8ce4d524550dde6c2a7e6
Ruben Aguilar <ragui56@wgu.edu>
2026-01-07 23:42:51 -0800

    install embla fade carousel, add carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import EmblaCarousel from "./sections/EmblaCarousel"
 import Header from "./sections/Header"
 
 const App = () => {
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Header />
+      <EmblaCarousel />
     </div>
   )
 }
