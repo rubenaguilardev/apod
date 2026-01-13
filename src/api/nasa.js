872eb585d5458bc0c014fcfa07fadbe526cecfcf
Ruben Aguilar <ragui56@wgu.edu>
2026-01-12 23:55:06 -0800

    add range fetch

--- src/api/nasa.js
+++ src/api/nasa.js
@@ -1,6 +1,7 @@
 const apiKey = 'DEMO_KEY'
 const baseUrl = 'https://api.nasa.gov/planetary/apod'
 
+
 export const fetchApod = async () => {
     
     const url = `${baseUrl}?api_key=${apiKey}`
@@ -13,6 +14,25 @@ export const fetchApod = async () => {
         return await res.json()
     } catch(err) {
         console.error('fetchApod error:', err)
+        throw err
+    }
+}
+
+
+export const fetchApodRange = async (days) => {
+
+    const url = `${baseUrl}?api_key=${apiKey}&end_date=${endDate}&start_date=${startDate}`
+    const startDate = '2026-01-08'
+    const endDate = '2026-01-12'
+
+    try {
+        const res = await fetch(url)
+        if (!res.ok) {
+            throw new Error('failed to fectch APOD range')
+        }
+        return await res.json()
+    } catch(err) {
+        console.error('fetchApodRange error:', err)
         throw err
     }
 }
